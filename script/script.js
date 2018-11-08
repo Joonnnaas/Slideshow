9efb62bc5c049c28591d117accd2a35265b3030a
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2018-11-08 20:42:44 +0100

    html, css finished - js is not working yet

---
+++ script/script.js
@@ -0,0 +1,14 @@
+function goLeft() {
+  const q0 = document.getElementById('Queen1');
+  const q1 = document.getElementById('Queen2');
+  const q2 = document.getElementById('Queen3');
+  const q3 = document.getElementById('Queen4');
+  const q4 = document.getElementById('Queen5');
+}
+function goRight() {
+  const q0 = document.getElementById('Queen1');
+  const q1 = document.getElementById('Queen2');
+  const q2 = document.getElementById('Queen3');
+  const q3 = document.getElementById('Queen4');
+  const q4 = document.getElementById('Queen5');
+}
